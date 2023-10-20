35622bd3059395c72cbc9152e8d6ffa84321e5e1
Bharathan <bharathan655@gmail.com>
2023-10-20 18:45:15 +0530

    Description deleted

--- script.js
+++ script.js
@@ -24,12 +24,9 @@ const getProduct = (product) =>{
     return `
         <div class="product">
         <img class="product-img" src="${product.image}">
-        <h2 class="product-title">${title.length > 18 ? title.substring(0, 18).concat(' ...') : title}</h2>
+        <h2 class="product-title">${title}</h2>
         <h4 class="product-category">${product.category}</h4>
-        <p class="product-description">${description.length > 80
-              ? description.substring(0, 80).concat(' ...more')
-              : description
-          }</p>
+       
         <h3 class="product-price">$${product.price}</h3>
         </div>
     `
